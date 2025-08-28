59b370c0b5d1c5fe77312b8f7a34133e2fba72e4
ASUS TUF <erkinov.0021@gmail.com>
2025-08-29 01:41:18 +0500

    Landing page added

--- hosilim/src/App.js
+++ hosilim/src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-import Home from './pages/Home';
-import './App.css';
+import LandingPage from './pages/LandingPage';
+// Boshqa sahifalar, agar mavjud bo‘lsa
 
 const App = () => {
   return (
     <Router>
-      <Header userType="Punktchi" userName="Anvar Qodirov" />
-      <div className="container">
-        <Sidebar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </main>
-      </div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        {/* Boshqa routelar */}
+      </Routes>
     </Router>
   );
 };
