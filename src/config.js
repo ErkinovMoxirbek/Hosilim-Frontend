7b41d7748b3d7cb3b917a676f3fbe8c1e8252fca
ASUS TUF <erkinov.0021@gmail.com>
2026-04-26 10:06:14 +0500

    fixed

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = "https://tasty-rats-add.loca.lt/api/v1";//https://tasty-rats-add.loca.lt
+const API_BASE_URL = "https://e19b3e46c0dd35.lhr.life/api/v1";//https://tasty-rats-add.loca.lt
 export default API_BASE_URL;
